0744c1e5dd645411c1d013859a2b2051154ade03
Sasha <sasha.panchyshun@gmail.com>
2025-04-30 18:58:57 +0300

    docker starter

---
+++ src/test/java/doji/doe/carsharing/CarSharingAppTests.java
@@ -0,0 +1,12 @@
+package doji.doe.carsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppTests {
+
+    @Test
+    void contextLoads() {}
+
+}
